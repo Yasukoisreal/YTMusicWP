b594b9cd98a0a3dc56535e00f2d4f83da3dd10a0
Yasuko <nguyentruongan06052007@gmail.com>
2026-06-18 22:14:21 +0700

    fix: Add missing Windows.Storage using in MainPage.Artist.cs

--- YTMusicWP/Pages/MainPage.Artist.cs
+++ YTMusicWP/Pages/MainPage.Artist.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
